ac11f5b451107c62a11f4e03598549d50f44e5c8
ilya-vor <157091996+ilya-vor@users.noreply.github.com>
2024-03-01 16:09:35 +0500

    Динамическое и статическое создание объекта

--- LW2_OOP.cpp
+++ LW2_OOP.cpp
@@ -1,7 +1,48 @@
 ﻿#include <iostream>
 
+class Point {
+private:
+    int x, y;
+public:
+    Point() {
+        printf("Point()\n");
+        x = 0;
+        y = 0;
+    }
+    Point(int x, int y) {
+        x = x;
+        y = y;
+    }
+    Point(Point& p) {
+        x = p.x;
+        y = p.y;
+    }
+    void show() {
+        printf("x = %d, y = %d\n", x, y);
+    }
+    ~Point() {
+        printf("~Point()\n");
+    }
+};
+
 int main()
 {
-    printf("Hello World!\n");
+    setlocale(NULL,"RU");
+
+    // Динамическое создание точки и ее удаление
+    printf("\nДинамическое создание точки и ее удаление:\n");
+    Point* p = new Point();
+    p->show();
+    delete(p);
+    p->show();
+
+    // Статическое создание точки и ее удаление
+    printf("\nСтатическое создание точки и ее удаление:\n");
+    {
+        Point p;
+        p.show();
+    }
+    p->show();
+
 }
 
